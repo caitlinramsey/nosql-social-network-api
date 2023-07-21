84900567a25f6cbee0d0f8e9b61174745c26712a
Caitlin <caitlinramsey@outlook.com>
2023-07-21 13:42:30 -0400

    added thought-routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,25 @@
 const router = require("express").Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require("../../controllers/thought-controller.js");
+
+// /api/thoughts
+router.route("/"),get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:id/reactions
+router.route("/:id/reactions").post(createReaction);
+
+// /api/thoughts/:id/reactions/:reactionId
+router.route("/:id/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
